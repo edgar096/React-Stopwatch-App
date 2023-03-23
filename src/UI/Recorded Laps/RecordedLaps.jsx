ac074a209ac063b9de82ffb5388a530b354ea226
edgar096 <edgar.goncalves96@gmail.com>
2023-03-23 16:45:52 +0000

    2 files modified

--- src/UI/Recorded Laps/RecordedLaps.jsx
+++ src/UI/Recorded Laps/RecordedLaps.jsx
@@ -1,5 +1,13 @@
-import { List } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 
+const style = {
+  width: '100%',
+  maxWidth: 360,
+  //bgcolor: 'background.paper',
+};
 const RecordedLaps = ({ laps }) => {
   return (
     <ul>
@@ -7,6 +15,14 @@ const RecordedLaps = ({ laps }) => {
         <li key={lap.id}>{lap.value}</li>
       ))}
     </ul>
+    // <List sx={style} component="nav" aria-label="mailbox folders">
+    //   {laps.map((lap) => {
+    //     <ListItem>
+    //       <ListItemText key={lap.id} primary={lap.value} />;
+    //     </ListItem>;
+    //     <Divider />;
+    //   })}
+    // </List>
   );
 };
 export default RecordedLaps;
