162fef77268583093a8860ba7bb063dfe9c2f310
edgar096 <edgar.goncalves96@gmail.com>
2023-03-29 14:24:46 +0100

    Resetting CSS styling from default Vite App

--- src/UI/Buttons/StartButton.jsx
+++ src/UI/Buttons/StartButton.jsx
@@ -4,9 +4,13 @@ const StartButton = ({ handleStart, handleStop, isClicked }) => {
   return (
     <div>
       {isClicked ? (
-        <Button onClick={handleStop}>Reset Timer</Button>
+        <Button variant="contained" onClick={handleStop}>
+          Reset Timer
+        </Button>
       ) : (
-        <Button onClick={handleStart}>Start Timer</Button>
+        <Button variant="contained" onClick={handleStart}>
+          Start Timer
+        </Button>
       )}
     </div>
   );
--- src/UI/Card/Card.jsx
+++ src/UI/Card/Card.jsx
@@ -1,8 +1,15 @@
 import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
 
 const Card = ({ children }) => {
   return (
-    <Stack direction="row" spacing={2}>
+    <Stack
+      direction="row"
+      divider={<Divider orientation="vertical" flexItem />}
+      spacing={2}
+      alignItems="center"
+      justify="flex-end"
+    >
       {children}
     </Stack>
   );
